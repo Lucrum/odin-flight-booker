209653e92ad773c42620af9cec61a6ea6f0a6770
Elton <28992495+Lucrum@users.noreply.github.com>
2023-10-08 17:48:03 -0400

    Flights, Airports, associations, index

---
+++ db/migrate/20231008173431_add_index_to_airport_code.rb
@@ -0,0 +1,5 @@
+class AddIndexToAirportCode < ActiveRecord::Migration[7.0]
+  def change
+    add_index :airports, :code, unique: true
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airport_codes = %w[ATL DFW DEN ORD DXB LAX
+                   IST LHR DEL CDG JFK LAS]
+
+airport_codes.each do |c|
+  Airport.create!({ code: c })
+end
+
+p "Created #{airport_codes.length} airports"
+
+random_flights_amount = 10
+
+random_flights_amount.times do
+  depart, arrive = airport_codes.sample(2)
+  # + 1 since database IDs start at 1
+  depart_id = airport_codes.index(depart) + 1
+  arrive_id = airport_codes.index(arrive) + 1
+  # puts "departing from: #{depart}, #{depart_id} arriving to: #{arrive}, #{arrive_id}"
+  Flight.create!({ start: DateTime.now + rand(2..60), flight_duration: rand(120..1440),
+                   departure_airport_id: depart_id, arrival_airport_id: arrive_id })
+end
+
+p "Created #{random_flights_amount} flights"
